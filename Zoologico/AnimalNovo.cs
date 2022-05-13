c710030e72eb13ec7f00489e66e379bef078a1c8
João Marcelo <joao_marcelo.22@hotmail.com>
2022-05-13 12:08:07 -0300

    feat: :zap: Arquivos Poo exercicio zoologico

---
+++ Zoologico/AnimalNovo.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Zoologico
+{
+    public class AnimalNovo : Animal
+    {
+        public AnimalNovo(string especie,double peso, string tipoAlimentacao):base(especie,peso)
+        {
+            this.TipoAlimentacao = tipoAlimentacao;
+        }
+        public string TipoAlimentacao { get; set; }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Especide : "{this.Especie}"Peso : " {this.Peso} "Alimentação : "{this.TipoAlimentacao});
+        }
+    }
+}
---
+++ Zoologico/Logica.cs
@@ -0,0 +1,23 @@
+namespace Zoologico
+{
+    public class Logica
+    {
+        int ErbivoroQtd = 0;
+        int CarnivoroQrd = 0;
+        public static void Erbivoro()
+        {
+            if(TipoAlimentacao == Erbivoro)
+            {
+                ErbivoroQtd++;
+            }
+        }
+        public static void Carnivoro()
+        {
+            if(TipoAlimentacao == Carnivoro)
+            {
+                CarnivoroQtd++;
+            }
+
+        }
+    }
+}
