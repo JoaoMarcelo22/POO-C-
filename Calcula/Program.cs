465a6af0a3a04afc7eb160c5e0103ad53d3464ec
João Marcelo <joao_marcelo.22@hotmail.com>
2022-05-17 14:50:51 -0300

    feat: :sparkles: Calculadora

---
+++ Calcula/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Calcula
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+            var b = 10;
+            var a = 25;
+            var total = a+b;
+            Console.WriteLine(total);
+        }
+    }
+}
