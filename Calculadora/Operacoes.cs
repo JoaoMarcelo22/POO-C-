36f03be1761d585a325144f2c051dbcd0add8354
João Marcelo <joao_marcelo.22@hotmail.com>
2022-05-17 22:03:27 -0300

    refactor: :sparkles:

--- Calculadora/Operacoes.cs
+++ Calculadora/Operacoes.cs
@@ -71,6 +71,13 @@ namespace Calculadora
         }
         public static void Subtracao()
         { 
+            Texto();
+
+            float resultado = N1 - N2;
+            
+        }
+        public static void Texto()
+        {
             Console.Clear();
             Console.WriteLine("Digite o primeiro numero : ");
             float.TryParse(Console.ReadLine(),out float N1);
@@ -78,11 +85,9 @@ namespace Calculadora
             Console.WriteLine("Digite o segundo numero : ");
             float.TryParse(Console.ReadLine(),out float N2);
 
-            float resultado = N1 - N2;
-             
             Console.WriteLine("");
 
-            Console.WriteLine($"O resultado da subtração é : {resultado}");
+            Console.WriteLine($"O resultado é : {resultado}");
             Console.ReadKey();
             Menu();
         }
